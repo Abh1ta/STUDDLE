dfc1920430167d5faa38417cc37a6f90bf7f5df7
AdrianaMor123 <adrianacrina2005@gmail.com>
2026-04-26 18:44:45 +0300

    creare parte de backend pentru login/signin

--- models/userModel.js
+++ models/userModel.js
@@ -6,7 +6,7 @@ const userSchema = new mongoose.Schema({
     email: { type: String, required: true, unique: true },
     password_hash: { type: String, required: true },
     avatar_url: { type: String },
-    xp: { type: Number, default: 0 },
+    xp: { type: Number, default: 100 },
     level: { type: Number, default: 1 },
     streak_days: { type: Number, default: 0 },
     last_active: { type: Date, default: Date.now }
--- server.js
+++ server.js
@@ -1,14 +1,23 @@
 import express from 'express';
 import cors from 'cors';
+import connectDB from './config/mongodb.js';
+import authRoutes from './routes/authRoutes.js';
 import 'dotenv/config'
 
 //app config
 const app = express()
 const port = process.env.PORT || 4000
+
+// connect to MongoDB
+connectDB();
+
 //middleware
 app.use(express.json())
 app.use(cors())
+
 //api endpoints
+app.use("/api/auth", authRoutes);
+
 app.get('/', (req, res) => {
     res.status(200).send('Hello World!')
 })
