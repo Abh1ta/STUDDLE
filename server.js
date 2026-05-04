28fa68c3c5adf9de38172b144063260ba6f3e05b
AdrianaMor123 <adrianacrina2005@gmail.com>
2026-05-04 13:02:07 +0300

    added the achivement parts

--- server.js
+++ server.js
@@ -3,21 +3,28 @@ import express from 'express';
 import cors from 'cors';
 import connectDB from './config/mongodb.js';
 import authRoutes from './routes/authRoutes.js';
-
-
+import fileRoutes from "./routes/fileRoutes.js";
+import connectCloudinary from './config/cloudinary.js'
+import settingsRoutes from "./routes/settingsRoutes.js";
+import friendshipRoutes from "./routes/friendshipRoutes.js";
+import achievementRoutes from "./routes/achievementRoutes.js";
+import userAchievementRoutes from "./routes/userAchievementRoutes.js";
 //app config
 const app = express()
 const port = process.env.PORT || 4000
-
-// connect to MongoDB
-connectDB();
-
+connectDB()
+connectCloudinary()
 //middleware
 app.use(express.json())
-app.use(cors())
+app.use(cors()) 
 
 //api endpoints
+app.use("/api/settings", settingsRoutes);
 app.use("/api/auth", authRoutes);
+app.use("/api/friends", friendshipRoutes);
+app.use("/api/achievements", achievementRoutes);
+app.use("/api/user-achievements", userAchievementRoutes);
+app.use("/api/files", fileRoutes);
 
 app.get('/', (req, res) => {
     res.status(200).send('Hello World!')
