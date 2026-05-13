023d3f5f4c1faaf8da0219dddd51c4e04f96309d
Cristiana2512 <ionitacristiana123@gmail.com>
2026-05-13 23:42:07 +0300

    Configurat rutele aplicației și basename pentru deploy

---
+++ frontend/src/main.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import App from './pages/App.jsx'
+import StartPage from './pages/StartPage.jsx'
+import Login from './pages/Login.jsx'
+import SignUp from './pages/SignUp.jsx'
+import FriendsPage from './pages/FriendsPage.jsx'
+import Settings from './pages/Settings.jsx'
+import ChatRoute from './pages/ChatRoute.jsx'
+import './index.css'
+import { AuthProvider } from './context/AuthContext.jsx'
+import { SocketProvider } from './context/SocketContext.jsx'
+import PaginaMateriale from './pages/PaginaMateriale.jsx'
+import Layout from './Layout.jsx'
+import TimerSelection from './pages/TimerSelection.jsx'
+import ActiveTimer from './pages/ActiveTimer.jsx'
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <AuthProvider>
+        <SocketProvider>
+          <Layout />
+          <Routes>
+            <Route path="/" element={<StartPage />} />     
+            <Route path="/home" element={<App />} />          
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="/friends" element={<FriendsPage />} />
+            <Route path="/settings" element={<Settings />} />
+            <Route path="/timer" element={<TimerSelection />} />
+            <Route path="/timer/:mode" element={<ActiveTimer />} />
+            <Route path="/chat" element={<ChatRoute />} />
+            <Route path="/materiale" element={<PaginaMateriale />} />
+          </Routes>
+        
+        </SocketProvider>
+      </AuthProvider>
+    </BrowserRouter>
+  </React.StrictMode>,
+)
