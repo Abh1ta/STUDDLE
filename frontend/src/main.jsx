949e6f1c8d6493252aa88def781203c0731d9e0b
madalinaStefaniaGh <madalinagh15@gmail.com>
2026-05-18 15:04:42 +0300

    fix: main.jsx conflicts resolved

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -12,10 +12,7 @@ import './index.css'
 import { AuthProvider } from './context/AuthContext.jsx'
 import { SocketProvider } from './context/SocketContext.jsx'
 import { AvatarProvider } from './context/AvatarContext.jsx'
-<<<<<<< HEAD
-=======
 import { SidebarProvider } from './context/SidebarContext.jsx'
->>>>>>> origin/feature/update
 import PaginaMateriale from './pages/PaginaMateriale.jsx'
 import Layout from './Layout.jsx'
 import TimerSelection from './pages/TimerSelection.jsx'
@@ -23,77 +20,44 @@ import ActiveTimer from './pages/ActiveTimer.jsx'
 import CharacterCustomization from './pages/CharacterCustomization.jsx'
 import MyAccount from './pages/MyAccount.jsx'
 import FriendsChat from './pages/FriendsChat.jsx'
-<<<<<<< HEAD
-import PDFViewer from './pages/PDFViewer.jsx';
-import PaginaEditMateriale from './pages/PaginaEditMateriale.jsx'
-=======
 import PDFViewer from './pages/PDFViewer.jsx'
 import PaginaEditMateriale from './pages/PaginaEditMateriale.jsx'
 import Leaderboard from './pages/Leaderboard.jsx'
->>>>>>> origin/feature/update
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <AuthProvider>
         <AvatarProvider>
-<<<<<<< HEAD
-          <SocketProvider>
-            <Layout />
-            <Routes>
-              
-              <Route path="/" element={<StartPage />} />     
-              <Route path="/home" element={<App />} />          
-              <Route path="/login" element={<Login />} />
-              <Route path="/signup" element={<SignUp />} />
-              <Route path="/friends" element={<FriendsPage />} />
-              <Route path="/settings" element={<Settings />} />
-              <Route path="/chat" element={<ChatRoute />} />
-              <Route path="/materiale" element={<PaginaMateriale />} />
-              <Route path="/timer" element={<TimerSelection />} />
-              <Route path="/timer/:mode" element={<ActiveTimer />} />
-              <Route path="/customization" element={<CharacterCustomization />} />
-              <Route path="/myaccount" element={<MyAccount />} />
-              <Route path="/friendschat" element={<FriendsChat />} />
-              <Route path="/pdf-viewer" element={<PDFViewer />} />
-              <Route path="/edit-material:id" element={<PaginaEditMateriale />} />
-            </Routes>
-          </SocketProvider>
-=======
           <SidebarProvider>
             <SocketProvider>
               <Routes>
-              {/* fara sidebar */}
-              <Route path="/" element={<StartPage />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="/signup" element={<SignUp />} />
-              <Route path="/pdf-viewer" element={<PDFViewer />} />
+                {/* Pagini FĂRĂ sidebar */}
+                <Route path="/" element={<StartPage />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/signup" element={<SignUp />} />
+                <Route path="/pdf-viewer" element={<PDFViewer />} />
 
-              {/*cu sidebar */}
-              <Route element={<Layout />}>
-                <Route path="/home" element={<App />} />
-                <Route path="/friends" element={<FriendsPage />} />
-                <Route path="/settings" element={<Settings />} />
-                <Route path="/chat" element={<ChatRoute />} />
-                <Route path="/materiale" element={<PaginaMateriale />} />
-                <Route path="/timer" element={<TimerSelection />} />
-                <Route path="/timer/:mode" element={<ActiveTimer />} />
-                <Route path="/customization" element={<CharacterCustomization />} />
-                <Route path="/myaccount" element={<MyAccount />} />
-                <Route path="/friendschat" element={<FriendsChat />} />
-                <Route path="/edit-material/:id" element={<PaginaEditMateriale />} />
-                <Route path="/leaderboard" element={<Leaderboard />} />
-              </Route>
-            </Routes>
+                {/* Pagini CU sidebar */}
+                <Route element={<Layout />}>
+                  <Route path="/home" element={<App />} />
+                  <Route path="/friends" element={<FriendsPage />} />
+                  <Route path="/settings" element={<Settings />} />
+                  <Route path="/chat" element={<ChatRoute />} />
+                  <Route path="/materiale" element={<PaginaMateriale />} />
+                  <Route path="/timer" element={<TimerSelection />} />
+                  <Route path="/timer/:mode" element={<ActiveTimer />} />
+                  <Route path="/customization" element={<CharacterCustomization />} />
+                  <Route path="/myaccount" element={<MyAccount />} />
+                  <Route path="/friendschat" element={<FriendsChat />} />
+                  <Route path="/edit-material/:id" element={<PaginaEditMateriale />} />
+                  <Route path="/leaderboard" element={<Leaderboard />} />
+                </Route>
+              </Routes>
             </SocketProvider>
           </SidebarProvider>
->>>>>>> origin/feature/update
         </AvatarProvider>
       </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>,
-<<<<<<< HEAD
 )
-=======
-)
->>>>>>> origin/feature/update
