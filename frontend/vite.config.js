a60ceb8daac4b73b9907fa626cc6162f6860c21b
madalinaStefaniaGh <madalinagh15@gmail.com>
2026-05-18 14:59:50 +0300

    fix: resolved vite.config.js merge conflicts

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-<<<<<<< HEAD
 // https://vite.dev/config/
-=======
-
->>>>>>> origin/feature/update
 export default defineConfig({
   plugins: [react()],
   server: {
